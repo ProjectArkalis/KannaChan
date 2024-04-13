5ccfdef2bb31c5ee083c57a3e343eec3e8f2d440
Yummiii <Leeo05050@protonmail.com>
2024-04-13 09:44:38 -0300

    sla, talvez mude isso dps

--- src/commands/utils/encode.rs
+++ src/commands/utils/encode.rs
@@ -3,18 +3,45 @@ use std::{
     process::{Command, Stdio},
 };
 
+use glob::glob;
+use tokio::fs;
+
 pub async fn encode(path: String, output: Option<String>) -> anyhow::Result<()> {
     //ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i input.mkv -c:v av1_nvenc -c:a libopus -b:v 8M output.webm
-    // Isso ta quebardo no momento
+    // Isso ta quebardo no
 
-    let output = output.unwrap_or(
-        Path::new(&path)
-            .with_extension("webm")
-            .to_str()
-            .unwrap()
-            .to_string(),
-    );
+    if Path::new(&path).is_dir() {
+        for file in glob(&format!("{}/*", path))? {
+            let file = file?;
 
+            let output = file.with_extension("webm").to_str().unwrap().to_string();
+
+            ffmpeg(file.to_str().unwrap().to_owned(), output)?;
+
+            let old = format!("{}/old", path);
+            fs::create_dir_all(&old).await?;
+            fs::rename(
+                &file,
+                format!("{}/{}", old, file.file_name().unwrap().to_str().unwrap()),
+            )
+            .await?;
+        }
+    } else {
+        let output = output.unwrap_or(
+            Path::new(&path)
+                .with_extension("webm")
+                .to_str()
+                .unwrap()
+                .to_string(),
+        );
+
+        ffmpeg(path, output)?;
+    }
+
+    Ok(())
+}
+
+fn ffmpeg(file: String, out: String) -> anyhow::Result<()> {
     let mut cmd = Command::new("ffmpeg")
         .args([
             "-hwaccel",
@@ -22,14 +49,14 @@ pub async fn encode(path: String, output: Option<String>) -> anyhow::Result<()> 
             "-hwaccel_output_format",
             "cuda",
             "-i",
-            &path,
+            &file,
             "-c:v",
             "av1_nvenc",
             "-c:a",
             "libopus",
             "-b:v",
             "8M",
-            &output,
+            &out,
         ])
         .stdout(Stdio::piped())
         .spawn()?;
